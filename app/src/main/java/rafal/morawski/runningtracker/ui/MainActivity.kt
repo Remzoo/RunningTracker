1c932bf0d6770b935769274fc25d6114ca14f8b2
Rafal Morawski <morawski.raf@gmail.com>
2021-01-24 16:41:24 +0100

    Created foreground service

--- app/src/main/java/rafal/morawski/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/rafal/morawski/runningtracker/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package rafal.morawski.runningtracker.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import rafal.morawski.runningtracker.R
+import rafal.morawski.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -26,5 +30,16 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
