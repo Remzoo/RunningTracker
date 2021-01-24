3e39d89e90d74983731de001ae890d3533d6c827
Rafal Morawski <morawski.raf@gmail.com>
2021-01-24 20:58:29 +0100

    Working tracking service

--- app/src/main/java/rafal/morawski/runningtracker/services/TrackingService.kt
+++ app/src/main/java/rafal/morawski/runningtracker/services/TrackingService.kt
@@ -1,30 +1,68 @@
 package rafal.morawski.runningtracker.services
 
-import android.app.Notification
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import rafal.morawski.runningtracker.R
 import rafal.morawski.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
 import rafal.morawski.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import rafal.morawski.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import rafal.morawski.runningtracker.other.Constants.ACTION_STOP_SERVICE
+import rafal.morawski.runningtracker.other.Constants.LOCATION_FASTEST_INTERVAL
+import rafal.morawski.runningtracker.other.Constants.LOCATION_UPDATE_INTERVAL
 import rafal.morawski.runningtracker.other.Constants.NOTIFICATION_CHANNEL_ID
 import rafal.morawski.runningtracker.other.Constants.NOTIFICATION_CHANNEL_NAME
 import rafal.morawski.runningtracker.other.Constants.NOTIFICATION_ID
+import rafal.morawski.runningtracker.other.TrackingUtility
 import rafal.morawski.runningtracker.ui.MainActivity
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
 
     private var isFirstRun = true
+
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -35,12 +73,14 @@ class TrackingService : LifecycleService() {
                         isFirstRun = false
                     } else {
                         Timber.d("Resuming service...")
+                        startForegroundService()
                     }
                     Timber.d("Started or resumed service")
 
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
+                    pauseService()
                 }
                 ACTION_STOP_SERVICE -> {
                     Timber.d("Stopped service")
@@ -49,6 +89,55 @@ class TrackingService : LifecycleService() {
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+    private fun pauseService() {
+        isTracking.postValue(false)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = LOCATION_FASTEST_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationClient.requestLocationUpdates(request, locationCallback, Looper.getMainLooper())
+            }
+        } else {
+            fusedLocationClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+            if (isTracking.value!!) {
+                result?.locations?.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: $location")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
 
     private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
         this,
@@ -60,6 +149,9 @@ class TrackingService : LifecycleService() {
     )
 
     private fun startForegroundService() {
+        addEmptyPolyline()
+        isTracking.postValue(true)
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
