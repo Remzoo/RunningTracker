2a8cfa386983dceb8e2a721063d7ce3bae851d4b
Rafal Morawski <morawski.raf@gmail.com>
2021-01-24 14:50:37 +0100

    Navigation between fragments has been set

--- app/src/main/java/rafal/morawski/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/rafal/morawski/runningtracker/ui/MainActivity.kt
@@ -1,23 +1,30 @@
 package rafal.morawski.runningtracker.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import rafal.morawski.runningtracker.R
-import rafal.morawski.runningtracker.db.RunDAO
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDao: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("rundao", "onCreate: runDao ${runDao.hashCode()}")
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/rafal/morawski/runningtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/rafal/morawski/runningtracker/ui/fragments/RunFragment.kt
@@ -1,8 +1,12 @@
 package rafal.morawski.runningtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 import rafal.morawski.runningtracker.R
 import rafal.morawski.runningtracker.ui.viewmodels.MainViewModel
 
@@ -10,4 +14,12 @@ import rafal.morawski.runningtracker.ui.viewmodels.MainViewModel
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
